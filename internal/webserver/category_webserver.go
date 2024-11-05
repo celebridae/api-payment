7e9b25881c6051d9ba7792d6a89038752386baf8
Cesar Kwatchile Kaulipakua <cesar.kaulipakua@minfin.gov.ao>
2024-11-05 12:00:55 +0100

    feat: create webserver

--- internal/webserver/category_webserver.go
+++ internal/webserver/category_webserver.go
@@ -3,9 +3,12 @@ package webserver
 // webserver like controller
 
 import (
+	"api-payment/internal/entity"
 	"api-payment/internal/service"
 	"encoding/json"
 	"net/http"
+
+	"github.com/go-chi/chi"
 )
 
 type CategoryWebserver struct {
@@ -24,4 +27,39 @@ func (c *CategoryWebserver) GetCategories(resp http.ResponseWriter, req *http.Re
 		return
 	}
 	json.NewEncoder(resp).Encode(categories)
+}
+
+func (c *CategoryWebserver) GetCategoryById(resp http.ResponseWriter, req *http.Request) {
+	id := chi.URLParam(req, "id")
+
+	if id == "" {
+		http.Error(resp, "id is required", http.StatusBadRequest)
+		return
+	}
+
+	cat, err := c.CategoryService.GetCategory(id)
+	if err != nil {
+		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(resp).Encode(cat)
+}
+
+func (c *CategoryWebserver) PostCategory(resp http.ResponseWriter, req *http.Request) {
+	var cat entity.Category
+
+	err := json.NewDecoder(req.Body).Decode(&cat)
+	if err != nil {
+		http.Error(resp, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	result, err := c.CategoryService.PostCategories(&cat)
+	if err != nil {
+		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(resp).Encode(result)
+
 }
