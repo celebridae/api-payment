8681fbf8264ecafb4e6bacc0630a5fd09e82217f
Cesar Kwatchile Kaulipakua <cesar.kaulipakua@minfin.gov.ao>
2024-11-11 08:39:37 +0100

    feat: improve code main.go

--- cmd/catalog/main.go
+++ cmd/catalog/main.go
@@ -24,32 +24,39 @@ func main() {
 	}
 	defer db.Close()
 
+	router := initializeRoute(db)
+
+	fmt.Println("Server is running on port 85")
+	http.ListenAndServe(":333", router)
+
+}
+
+func initializeRoute(db *sql.DB) *chi.Mux {
+
 	categoryDB := database.NewCategoryDB(db)
-	categoryService := service.NewCategoryService(*categoryDB)
-	//categoryDB.GetCategories()
 	productDB := database.NewProductDB(db)
+
+	categoryService := service.NewCategoryService(*categoryDB)
 	productService := service.NewProductService(*productDB)
 
 	produWebservice := webserver.NewProductWebserver(*productService)
 	catWebservice := webserver.NewCategoryWebserver(*categoryService)
 
 	// Router
-	c := chi.NewRouter()
+	router := chi.NewRouter()
 	// setter all log
-	c.Use(middleware.Logger)
-	c.Use(middleware.Recoverer)
-	c.Get("/products/{id}", produWebservice.GetProduct)
-	c.Get("/products", produWebservice.GetProducties)
-	c.Get("/products/category/{id}", produWebservice.GetProductByCategory)
-	c.Post("/products", produWebservice.PostProduct)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Get("/products/{id}", produWebservice.GetProduct)
+	router.Get("/products", produWebservice.GetProducties)
+	router.Get("/products/category/{id}", produWebservice.GetProductByCategory)
+	router.Post("/products", produWebservice.PostProduct)
 
 	// category
-	c.Get("/category", catWebservice.GetCategories)
-	c.Get("/category/{id}", catWebservice.GetCategoryById)
-	c.Post("/category", catWebservice.PostCategory)
+	router.Get("/category", catWebservice.GetCategories)
+	router.Get("/category/{id}", catWebservice.GetCategoryById)
+	router.Post("/category", catWebservice.PostCategory)
 
-	fmt.Println("Server is running on port 85")
-
-	http.ListenAndServe(":333", c)
+	return router
 
 }
